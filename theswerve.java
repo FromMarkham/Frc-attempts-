92fc6528b3efa3415a98c85b2c19800bee932412
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-21 20:15:35 -0500

    Update theswerve.java

--- theswerve.java
+++ theswerve.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
-
+#https://drive.google.com/drive/folders/15jx3ApDzKKLP7ykGx_Ah46IjURASzL8w?usp=drive_link
+#https://www.youtube.com/watch?v=0Xi9yb1IMyA
 public class theswerve {
     private final CANSparkMax drivingmotor;
     private final CANSparkMax steeringmotor;
